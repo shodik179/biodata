1f68da403b202b44f962c801e1e605f678075955
shodik179 <shodik179@gmail.com>
2021-10-28 21:23:35 +0700

    menambahkan file biodata.py

---
+++ biodata.py
@@ -0,0 +1,13 @@
+print("===============")
+print("    biodata    ")
+print("===============")
+
+nama = input("nama lengkap:")
+panggilan = input("nama panggilan:")
+npm = input ("npm:")
+lahir = input ("tempat lahir:")
+tanggal = input ("tanggal lahir:")
+telpon = input ("no telpon:")
+alamat = input ("alamat:")
+
+print("hallo, perkenalkan nama saya %s bisa di panggilan %s npm saya %s tempat lahir saya di %s dan usia saya sekarang %s kalian dapat menghubungi saya melaluli no %s dan alamat tinggal saya di %s" % (nama, panggilan, npm, lahir, tanggal, telpon, alamat))
